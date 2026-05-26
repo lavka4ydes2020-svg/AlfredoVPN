4dea4bad029945e6a56b08836de1e29bf193a8b2
Ilnur Habiba <lavka4ydes2020-svg@users.noreply.github.com>
2026-05-26 23:24:56 +0500

    chore: bump to 2.2.6 for progress-bar test

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.alfredovpn"
         minSdk = 24
         targetSdk = 37
-        versionCode = 735
-        versionName = "2.2.5"
+        versionCode = 736
+        versionName = "2.2.6"
         multiDexEnabled = true
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
